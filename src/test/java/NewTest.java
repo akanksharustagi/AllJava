0d66ccdf17620fa2d10f5c16bcb61202c1ca1655
akanksharustagi <ena.akanksha@gmail.com>
2022-05-31 13:12:46 -0400

    added quit

--- src/test/java/NewTest.java
+++ src/test/java/NewTest.java
@@ -4,13 +4,15 @@ import org.openqa.selenium.chrome.ChromeDriver;
 
 public class NewTest {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
         System.setProperty("webdriver.chrome.driver","C:\\Users\\enaak\\Downloads\\chromedriver102\\chromedriver.exe");
         WebDriver driver = new ChromeDriver();
         driver.get("https://www.amazon.ca/");
         driver.findElement(By.id("twotabsearchtextbox")).sendKeys("camera");
         driver.findElement(By.id("nav-search-submit-button")).click();
         System.out.println("Test successfully done");
+        driver.quit();
+
     }
 
 
