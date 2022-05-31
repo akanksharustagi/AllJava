211fe2de2dfa8386987e7812ce959949ba37068b
akanksharustagi <ena.akanksha@gmail.com>
2022-05-31 13:17:47 -0400

    renamed test

--- src/test/java/amazonTest.java
+++ src/test/java/amazonTest.java
@@ -2,7 +2,7 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
-public class NewTest {
+public class amazonTest {
 
     public static void main(String[] args) throws InterruptedException {
         System.setProperty("webdriver.chrome.driver","C:\\Users\\enaak\\Downloads\\chromedriver102\\chromedriver.exe");
